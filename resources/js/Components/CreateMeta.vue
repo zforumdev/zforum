b70c61fc51a0afa71ade6bd65a4a37daa616110a
Ashlyn <ash@midiidev.xyz>
2023-05-29 22:51:00 -0400

    escape in meta

--- resources/js/Components/CreateMeta.vue
+++ resources/js/Components/CreateMeta.vue
@@ -7,21 +7,29 @@ const props = defineProps([
     'description',
     'image',
 ])
+
+const escape = (str) => {
+    return str.replace(/&/g, "&amp;")
+        .replace(/>/g, "&gt;")
+        .replace(/</g, "&lt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g,"&apos;")
+}
 </script>
 
 <template>
     <Head>
         <title>{{ title }} - {{ config.name }}</title>
-        <meta name="title" :content="title">
-        <meta name="description" :content="description ?? config.description">
+        <meta name="title" :content="escape(title)">
+        <meta name="description" :content="escape(description ?? config.description)">
         <meta name="theme-color" content="#381e72">
         <meta property="og:type" content="website">
-        <meta property="og:url" :content="config.url">
-        <meta property="og:title" :content="`${title} - ${config.name}`">
-        <meta property="og:description" :content="description ?? config.description">
+        <meta property="og:url" :content="escape(config.url)">
+        <meta property="og:title" :content="escape(`${title} - ${config.name}`)">
+        <meta property="og:description" :content="escape(description ?? config.description)">
         <meta property="twitter:card" content="summary_large_image">
-        <meta property="twitter:url" :content="config.url">
-        <meta property="twitter:title" :content="`${title} - ${config.name}`">
-        <meta property="twitter:description" :content="description">
+        <meta property="twitter:url" :content="escape(config.url)">
+        <meta property="twitter:title" :content="escape(`${title} - ${config.name}`)">
+        <meta property="twitter:description" :content="escape(description)">
     </Head>
 </template>
